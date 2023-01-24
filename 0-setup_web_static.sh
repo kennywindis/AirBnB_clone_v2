907064232293f8d540071e8a68d673452a4c6745
kennywindis <kennyphilip100@gmail.com>
2023-01-24 00:23:36 -0800

    abc

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,41 @@
 #!/usr/bin/env bash
-# Script that sets up your web servers for the deployment of web_static.
+# installs nginx if not installed
 
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+if ! which nginx;
+then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
+
+# create folders if they don't exist
+if [[ ! -e /data/web_static/releases/test/ ]];
+then
+    sudo mkdir -p /data/web_static/releases/test/
+fi
+
+if [[ ! -e /data/web_static/shared/ ]];
+then
+    sudo mkdir -p /data/web_static/shared/
+fi
+
+# create dummy html file
 echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" >> /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+  </html>" > /data/web_static/releases/test/index.html
+
+# create symbolic link
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
+
+# chown of /data/ to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+
+# update nginx confi to serve content of /data/web_static/current to hbnb_static
+sudo sed -i '39i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+
+# restarts nginx
 sudo service nginx restart
 
