459f8dc492155cd7dc83f58840d61e0f5e802aac
kennywindis <kennyphilip100@gmail.com>
2023-01-24 01:54:16 -0800

    abc

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,68 +1,59 @@
 #!/usr/bin/python3
 '''
-fabric script to distribute an archive to web servers
-----NEEDS TO REVISIT SCRIPT
+    Script distributes an archive to
+    web servers through automation to both servers
 '''
-
+from fabric.api import *
 import os
-from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
 
-
-env.hosts = ['34.138.32.248', '3.226.74.205']
-
-
-def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
+# Servers's addresses
+env.hosts = ['100.26.133.225', '54.162.6.57']
 
 
 def do_deploy(archive_path):
-    """Deploys the static files to the host servers.
-    Args:
-        archive_path (str): The path to the archived static files.
-    """
-    if not os.path.exists(archive_path):
+    '''
+        Deploy web static content to web server
+    '''
+    if os.path.isfile(archive_path) is False:
         return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
+
+    start = 'versions'
+    print("Starting deployment")
+
     try:
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
-        print('New version deployed!')
-        success = True
-    except Exception:
-        success = False
-    return success
+        # Get the archive file through the file path
+        filename = archive_path.split("/")[-1]
+        no_ext = filename.split(".")[0]
+        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
+        symlink = "/data/web_static/current"
+
+        print("Uploading to /tmp/{}".format(filename))
+        # Upload the archive to /tmp/ dir web server
+        put(archive_path, "/tmp/{}".format(filename))
+
+        print("Creating directory {}".format(path_no_ext))
+        # Create directory on web server
+        run("mkdir -p {}".format(path_no_ext))
+
+        # Uncompress the archive to "folder"
+        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
+
+        # Delete archive file from web server
+        run("rm /tmp/{}".format(filename))
+
+        # Move content of web_static to "folder"
+        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+
+        # Delete the content of web_static
+        run("rm -rf {}web_static".format(path_no_ext))
+
+        # Delete symbolic link current on web server
+        run("rm -rf {}".format(symlink))
+
+        # Create new link: current and point to new version of code in "folder"
+        run("ln -s {} {}".format(path_no_ext, symlink))
+
+        print("New version deployed!")
+        return True
+    except Exception as err:
+        return False
