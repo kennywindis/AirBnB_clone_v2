c3941f1a3d246e586a1f1130bb8b097901312979
kennywindis <kennyphilip100@gmail.com>
2023-03-10 06:22:26 -0800

    sql

--- models/state.py
+++ models/state.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+import models
+from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from models.base_model import Base
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from models.city import City
 import os
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
+    __tablename__ = 'states'
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            'City',
+            cascade="all,delete-orphan",
+            backref="state")
+    else:
+        name = ""
 
-    @property
-    def cities(self):
-        """getter attribute cities that returns the list of City"""
-        from models import storage
-        my_list = []
-        extracted_cities = storage.all(City).values()
-        for city in extracted_cities:
-            if self.id == city.state_id:
-                my_list.append(city)
-        return my_list
+        @property
+        def cities(self):
+            """cities"""
+            cityArray = []
+            for item in models.storage.all(City).values():
+                if item.state_id == self.id:
+                    cityArray.append(item)
+            return cityArray
