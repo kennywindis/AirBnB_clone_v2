8c7a873e39a0a8c5620c04460278a1dd424b7136
kennywindis <kennyphilip100@gmail.com>
2023-03-10 22:28:46 -0800

    wf

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
+"""getting and rendering data from storage
 """
-@author: kehinde bandipo
-"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+from models.city import City
+import os
+from flask import g
+
+
 app = Flask(__name__)
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def render_state_obj():
+    """render html list"""
+    s = storage.all(State)
+    c = storage.all(City)
+    return render_template('8-cities_by_states.html', states=s, cities=c)
+
+
 @app.teardown_appcontext
-def appcontext_teardown(self):
-    """use storage for fetching data from the storage engine
-    """
+def teardown_db(self):
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def state_info():
-    """Display a HTML page inside the tag BODY"""
-    return render_template('8-cities_by_states.html',
-                           states=storage.all(State))
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0', port='5000')
