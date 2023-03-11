488d3f74a2dc08b66d5dd80b29f5cf106171891c
kennywindis <kennyphilip100@gmail.com>
2023-03-11 13:37:57 -0800

    abc

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,31 @@
-#!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
+#!/usr/bin/env python3
+"""Deploy webstack"""
+from fabric.api import *
+import os.path
+from os import path
 
-from fabric.api import put, run, env
-from os.path import exists
 env.hosts = ['3.84.158.146', '54.144.239.204']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """deploy to server"""
+    if path.exists('versions') is False:
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        f_n = archive_path[archive_path.find('/') + 1:archive_path.find('.')]
+        f_y = archive_path[archive_path.find('/') + 1:]
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(f_n))
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+            f_y, f_n))
+        run("sudo rm /tmp/{}".format(f_y))
+        run("sudo mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/".format(f_n, f_n))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static".format(f_n))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/\
+            /data/web_static/current".format(f_n))
         return True
-    except:
+    except Exception:
         return False
+    return False
