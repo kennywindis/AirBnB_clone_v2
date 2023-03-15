a4cd38dcb89b15ca4c6e487c7d9c490b65b903b4
kennywindis <kennyphilip100@gmail.com>
2023-03-15 07:24:31 -0700

    ab3

--- models/state.py
+++ models/state.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, ForeignKey
-import os
-from models.city import City
-from sqlalchemy.orm import relationship
+""" holds class State"""
 import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    __tablename__ = 'states'
-    if (os.getenv('HBNB_TYPE_STORAGE') != 'db'):
-        name = ''
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
 
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
         @property
         def cities(self):
-            """
-            Info
-            """
-            new_list = []
-            all_entries = models.storage.all(City)
-            for key, value in all_entries.items():
-                if self.id == value.state_id:
-                    new_list.append(value)
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
 
-            return new_list
-    else:
-        name = Column("name", String(128), nullable=False)
-        cities = relationship("City", cascade='all, delete', backref='state')
